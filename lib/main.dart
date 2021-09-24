e19ab93b0b2ec78bf1f57812b90b14c45e83450d
rodrigoota <rodrigotety@hotmail.com>
2021-09-23 22:47:09 -0300

    aula03

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:app_filmes/application/ui/filmes_app_ui_config.dart';
+import 'package:app_filmes/modules/home/home_module.dart';
 import 'package:app_filmes/modules/login/login_module.dart';
 import 'package:app_filmes/modules/splash/splash_module.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -20,15 +22,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: FilmesAppUiConfig.title,
       initialBinding: ApplicationBindings(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: FilmesAppUiConfig.theme,
       getPages: [
         ...SplashModule().routers,
         ...LoginModule().routers,
+        ...HomeModule().routers
       ],
     );
   }
 }
+
+class Home {}
